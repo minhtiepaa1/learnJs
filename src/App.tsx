a5e9538d701ae46790751ffddd180074795cee6d
minhtiep <minhtiepaa1@gmail.com>
2023-10-02 22:39:11 +0700

    update State

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import MyTable from "./components/MyTable";
+import { Transfer } from "antd";
+import State from "./container/hook/State";
 
 function App() {
   return (
     <div className="App">
-      <body>
+      <div>
         <MyTable />
-      </body>
-      <header className="App-header">
+
+        <input type="number" min={0} max={9999999} />
+        <State />
+      </div>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -22,7 +27,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
